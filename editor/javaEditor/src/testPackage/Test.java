f08cbceacb951e1e1afcd6e81e2c6d323d22a2ca
TwilCynder <twilcynder@gmail.com>
2021-12-23 05:37:01 +0100

    java gamedata editor (project setup)

---
+++ editor/javaEditor/src/testPackage/Test.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+import javax.swing.JFrame;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test Java VSC !");
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+    }
+}
