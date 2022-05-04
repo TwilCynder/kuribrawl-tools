bac43a716a3eda8fec1dded95b18a49635960458
TwilCynder <twilcynder@gmail.com>
2022-05-04 03:49:59 +0200

    number-only textfield/document

---
+++ editor/javaEditor/src/UI/TwilDocumentFilter.java
@@ -0,0 +1,18 @@
+package UI;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+public class TwilDocumentFilter extends DocumentFilter {
+    protected static String getCurrentString(FilterBypass fb) {
+        try {
+            return fb.getDocument().getText(0, fb.getDocument().getLength());
+        } catch (BadLocationException ex){
+            throw new IllegalStateException("Document " + fb.getDocument() + " threw a BadLocationException when trying to obtain text in range {offset : 0; length: getLength()}", ex);
+        }
+        
+    }
+
+    protected static String replace(String original, String added, int offset, int len){
+        return original.substring(0, offset) + added + original.substring(offset + len);
+    }
+}
