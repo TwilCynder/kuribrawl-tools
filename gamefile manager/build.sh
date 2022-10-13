25bc6c06f3e67d4dc9e274e21111cf7e47c46d0e
TwilCynder <twilcynder@gmail.com>
2022-10-13 13:44:44 +0200

    gamefile manager build script

--- gamefile manager/build.sh
+++ gamefile manager/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-echo "$parent_path"
-pbcompiler
+#parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+out="./DFM.exe"
+
+while getopts ":hi" option; do
+  case $option in
+    h) echo "usage: $0 [-h] [-i]"; exit ;;
+    i) out="../../res/DFM.exe" ;;
+    ?) echo "error: option -$OPTARG is not implemented"; exit ;;
+  esac
+done
+
+echo $out
+
+pbcompiler src/dataFileMaker.pb -e $out //ICON "../../GraphicDesignIsMyPassion/iconDev.ico" //CONSOLE
