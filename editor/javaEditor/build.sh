c4f8184cf790e6786f6c80e1f127f512b355cd7b
TwilCynder <twilcynder@gmail.com>
2022-11-21 15:22:18 +0100

    ant build (dependant jar+zip) + moved libs

---
+++ editor/javaEditor/build.sh
@@ -0,0 +1,5 @@
+mkdir classes
+javac -d ./classes -cp "./src;./lib/*" ./src/testPackage/Test.jav
+echo Main-Class: testPackage.Test>manifest.mf
+echo Class-Path: ./lib/*>manifest.mf
+jar cvfm kuribrawl-editor-test.jar manifest.mf -C classes .
