f543a0d1f20b98278e7e5f53e1f8a9c5c4960b7c
TwilCynder <twilcynder@gmail.com>
2023-01-27 10:15:30 +0100

    make build + ant build improvement

--- editor/javaEditor/build.sh
+++ editor/javaEditor/build.sh
@@ -1,5 +1,5 @@
 mkdir classes
 javac -d ./classes -cp "./src;./lib/*" ./src/testPackage/Test.java
 echo Main-Class: testPackage.Test>manifest.mf
-echo Class-Path: ./lib/*>manifest.mf
-jar cvfm kuribrawl-editor-test.jar manifest.mf -C classes .
+echo Class-Path: ./lib/*>>manifest.mf
+jar cvfm kuribrawl-editor.jar manifest.mf -C classes .
