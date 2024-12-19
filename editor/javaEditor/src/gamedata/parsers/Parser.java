30df6e3163284cd80931d2df1192e09de7c31ae5
TwilCynder <twilcynder@gmail.com>
2024-12-19 17:20:35 +0100

    use Parser.parseX to parse hitbox fields

--- editor/javaEditor/src/gamedata/parsers/Parser.java
+++ editor/javaEditor/src/gamedata/parsers/Parser.java
@@ -11,11 +11,27 @@ public class Parser {
         }
     }
 
+    public static int parseInt(String str, String numberDescription) throws RessourceException{
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e){
+            throw new RessourceException(numberDescription + " is not a valid integer", e);
+        }
+    }
+
     public static double parseDouble(String str, String numberDescription, String filename, int line) throws RessourceException{
         try {
             return Double.parseDouble(str);
         } catch (NumberFormatException e){
             throw new RessourceException(numberDescription + " is not a valid number", filename, line, e);
+        }
+    }
+
+    public static double parseDouble(String str, String numberDescription) throws RessourceException{
+        try {
+            return Double.parseDouble(str);
+        } catch (NumberFormatException e){
+            throw new RessourceException(numberDescription + " is not a valid number", e);
         }
     }
 }
