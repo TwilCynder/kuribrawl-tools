3e456eb3204f5918766459e00f192ef08dc0e80d
TwilCynder <twilcynder@gmail.com>
2025-08-09 04:34:56 +0200

    Damage hitbox use proper default values for kb

--- editor/javaEditor/src/gamedata/DamageHitbox.java
+++ editor/javaEditor/src/gamedata/DamageHitbox.java
@@ -5,8 +5,8 @@ import KBUtil.Rectangle;
 public class DamageHitbox extends Hitbox {
     public double damage = 0;
     public int angle = 0;
-    public double base_knockback = 0;
-    public double scaling_knockback = 0;
+    public double base_knockback = 1;
+    public double scaling_knockback = 1;
     public int hitID = 0;
     public int priority = 0;
     public AngleMode angle_mode = AngleMode.NORMAL;
